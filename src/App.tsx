6d92ebb514d363c6611999440569cfb90ac65685
Muhammad Hamka <hamkarifai49@gmail.com>
2025-10-18 05:48:58 +0700

    feat: add scroll to top components

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import About from "./pages/About";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
 import Dashboard from "./pages/Dasboard";
+import { ScrollToTop } from "./components/ScrollToTop";
 
 const queryClient = new QueryClient();
 
@@ -21,6 +22,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           <Route element={<Layout />}>
             <Route path="/" element={<Home />} />
